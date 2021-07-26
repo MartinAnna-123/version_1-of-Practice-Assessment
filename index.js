f26f863d7da327bd81c2135ea676763750b8bc9a
Anna Martin <78177123+MartinAnna-123@users.noreply.github.com>
2021-07-26 22:04:12 +0000

    attempted to add point system

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 // // let player2 = prompt('enter is player twos name?')
 // console.log('welcome to the game ' + player1)
 play();
+
 function play(){
+let pointPlayer1 = 0
+let pointPlayer2 = 0
+
 diceQuestion1();
 function diceQuestion1(){
 var play = prompt('player1 would you like to roll the dice?')
@@ -19,6 +23,7 @@ var play = prompt('player1 would you like to roll the dice?')
 function roll(){
   let dice1 = Math.floor(Math.random() * 6 + 1)
   console.log('you rolled a ' + dice1)
+  pointPlayer1 ++
 }
 
 diceQuestion2();
@@ -37,8 +42,9 @@ var play = prompt('player2 would you like to roll the dice?')
 function roll2(){
   let dice2 = Math.floor(Math.random() * 6 + 1)
   console.log('you rolled a ' + dice2)
+  pointPlayer2 ++
 }
-}
+
 var play2 = prompt ('do you want to play again?')
   if (play2 == 'yes'){
   play();
@@ -47,12 +53,13 @@ ending();
   }
 
 function ending(){
-if( dice1 > dice2 ){
+if( pointPlayer1 > pointPlayer2 ){
   console.log('player1 wins')
-}else if(dice1 = dice2){
+}else if(pointPlayer1 = pointPlayer2){
   console.log('draw')
 }else{
   console.log('player2 wins')
 }}
+}
 
 
